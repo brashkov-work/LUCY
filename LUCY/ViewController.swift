a6c1f7248c14f3c75a3656a100a878e9417c620b
Boris Rashkov <boris.rashkov@edentechlabs.io>
2019-02-15 22:11:02 +0200

    Initial Commit

---
+++ LUCY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LUCY
+//
+//  Created by Boris Rashkov on 15.02.19.
+//  Copyright © 2019 Boris Rashkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
